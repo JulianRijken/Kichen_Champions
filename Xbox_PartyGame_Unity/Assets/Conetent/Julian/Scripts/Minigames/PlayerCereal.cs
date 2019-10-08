03f1d84ff2d824bd750f132ee63f56a850a9fac0
Julian <Rijkenjulian@gmail.com>
2019-10-08 20:20:59 +0200

    Sprites added to cereal

--- Xbox_PartyGame_Unity/Assets/Conetent/Julian/Scripts/Minigames/PlayerCereal.cs
+++ Xbox_PartyGame_Unity/Assets/Conetent/Julian/Scripts/Minigames/PlayerCereal.cs
@@ -9,9 +9,13 @@ public class PlayerCereal : MonoBehaviour
     [SerializeField] private int m_player;
 
     [Header("Box")]
-    [SerializeField] private Transform m_cerealBox;
+    [SerializeField] private Rigidbody2D m_cerealBox;
     [SerializeField] private Transform m_cerealSpawnPoint;
     [SerializeField] private GameObject m_cerealPrefab;
+    [SerializeField] private float m_boxShakeAmmount;
+    [SerializeField] private float m_boxShakeStopSpeed;
+    [SerializeField] private float m_boxShakeSpeed;
+    [SerializeField] private float m_boxShake;
 
     [Header("Bole")]
     [SerializeField] private Rigidbody2D m_bole;
@@ -22,36 +26,57 @@ public class PlayerCereal : MonoBehaviour
 
     private Vector2 m_MovementInput;
     private Vector3 m_boleStartPos;
+    private Vector3 m_boxStartPos;
 
     void Start()
     {
         PlayerInputCenter.PlayerInputEvents[m_player].OnMove += HandeMovement;
+        PlayerInputCenter.PlayerInputEvents[m_player].OnButtonSouth += HandelButton;
 
+        m_boxShake = 0;
         m_boleStartPos = m_bole.transform.position;
+        m_boxStartPos = m_cerealBox.transform.position;
     }
 
     private void OnDestroy()
     {
         PlayerInputCenter.PlayerInputEvents[m_player].OnMove -= HandeMovement;
+        PlayerInputCenter.PlayerInputEvents[m_player].OnButtonSouth -= HandelButton;
     }
 
     private void HandeMovement(InputAction.CallbackContext conext)
     {
         m_MovementInput = conext.ReadValue<Vector2>();
     }
-
-   
-    private void Update()
+    
+    private void HandelButton(InputAction.CallbackContext conext)
     {
-        Instantiate(m_cerealPrefab, m_cerealSpawnPoint.transform.position, m_cerealSpawnPoint.transform.rotation);
+        if (conext.performed)
+        {
+            Instantiate(m_cerealPrefab, m_cerealSpawnPoint.transform.position, m_cerealSpawnPoint.transform.rotation);
+            m_boxShake = m_boxShakeAmmount;
+        }
+    }
 
+
+
+    private void MoveBox()
+    {
+        m_boxShake -= Time.deltaTime * m_boxShakeStopSpeed;
+        m_boxShake = Mathf.Clamp(m_boxShake,0, Mathf.Infinity);
+        Vector3 addedMove = new Vector3(Mathf.PerlinNoise(Time.time * m_boxShakeSpeed, 0) - 0.5f, 0, 0);
+        addedMove *= m_boxShake;
+        m_cerealBox.MovePosition(m_boxStartPos + addedMove);
     }
 
     private void FixedUpdate()
     {
         MoveBole();
+        MoveBox();
 
     }
+
+
 
     private void MoveBole()
     {
