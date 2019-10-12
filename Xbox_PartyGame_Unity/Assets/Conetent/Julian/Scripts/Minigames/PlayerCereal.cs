5ce1a70dddc61d2f105b7cb2d252437cb680163a
Julian <Rijkenjulian@gmail.com>
2019-10-12 12:44:21 +0200

    Work On Players

--- Xbox_PartyGame_Unity/Assets/Conetent/Julian/Scripts/Minigames/PlayerCereal.cs
+++ Xbox_PartyGame_Unity/Assets/Conetent/Julian/Scripts/Minigames/PlayerCereal.cs
@@ -11,14 +11,17 @@ public class PlayerCereal : MonoBehaviour
     [Header("Box")]
     [SerializeField] private Rigidbody2D m_cerealBox;
     [SerializeField] private Transform m_cerealSpawnPoint;
-    [SerializeField] private GameObject m_cerealPrefab;
+    [SerializeField] private string m_cerialName;
     [SerializeField] private float m_boxShakeAmmount;
     [SerializeField] private float m_boxShakeStopSpeed;
     [SerializeField] private float m_boxShakeSpeed;
-    [SerializeField] private float m_boxShake;
+    private float m_boxShake;
 
     [Header("Bole")]
     [SerializeField] private Rigidbody2D m_bole;
+    [SerializeField] private LayerMask m_cerialLayerMask;
+    [SerializeField] private float m_boleCheckRadius;
+    [SerializeField] private Vector2 m_boleCheckCenter;
     [SerializeField] private float m_swayScale;
     [SerializeField] private float m_swaySpeed;
     [SerializeField] private float m_noiseSwayScale;
@@ -27,6 +30,7 @@ public class PlayerCereal : MonoBehaviour
     private Vector2 m_MovementInput;
     private Vector3 m_boleStartPos;
     private Vector3 m_boxStartPos;
+    private float m_timer;
 
     void Start()
     {
@@ -36,6 +40,8 @@ public class PlayerCereal : MonoBehaviour
         m_boxShake = 0;
         m_boleStartPos = m_bole.transform.position;
         m_boxStartPos = m_cerealBox.transform.position;
+
+        m_timer = Random.Range(0f, 100f);
     }
 
     private void OnDestroy()
@@ -53,7 +59,7 @@ public class PlayerCereal : MonoBehaviour
     {
         if (conext.performed)
         {
-            Instantiate(m_cerealPrefab, m_cerealSpawnPoint.transform.position, m_cerealSpawnPoint.transform.rotation);
+            ObjectPooler.SpawnObject(m_cerialName, m_cerealSpawnPoint.transform.position, m_cerealSpawnPoint.transform.rotation,true);
             m_boxShake = m_boxShakeAmmount;
         }
     }
@@ -64,16 +70,29 @@ public class PlayerCereal : MonoBehaviour
     {
         m_boxShake -= Time.deltaTime * m_boxShakeStopSpeed;
         m_boxShake = Mathf.Clamp(m_boxShake,0, Mathf.Infinity);
-        Vector3 addedMove = new Vector3(Mathf.PerlinNoise(Time.time * m_boxShakeSpeed, 0) - 0.5f, 0, 0);
+        Vector3 addedMove = new Vector3(Mathf.PerlinNoise(m_timer * m_boxShakeSpeed, 0) - 0.5f, 0, 0);
         addedMove *= m_boxShake;
         m_cerealBox.MovePosition(m_boxStartPos + addedMove);
+    }
+
+    private void Update()
+    {
+        m_timer += Time.deltaTime;
+
+        Collider2D[] colliders = Physics2D.OverlapCircleAll((Vector2)m_bole.transform.position + m_boleCheckCenter, m_boleCheckRadius, m_cerialLayerMask);
+        Debug.Log(colliders.Length);
     }
 
     private void FixedUpdate()
     {
         MoveBole();
         MoveBox();
+    }
 
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(0, 1f, 0, 0.5f);
+        Gizmos.DrawSphere((Vector2)m_bole.transform.position + m_boleCheckCenter, m_boleCheckRadius);
     }
 
 
@@ -84,7 +103,7 @@ public class PlayerCereal : MonoBehaviour
 
         input.x *= m_swayScale;
 
-        input.x += (Mathf.PerlinNoise(Time.time * m_noiseSwaySpeed, Time.time * m_noiseSwaySpeed) - 0.5f) * m_noiseSwayScale;
+        input.x += (Mathf.PerlinNoise(m_timer * m_noiseSwaySpeed, m_timer * m_noiseSwaySpeed) - 0.5f) * m_noiseSwayScale;
 
         input.x = Mathf.Clamp(input.x, -m_swayScale, m_swayScale);
 
---
+++ Xbox_PartyGame_Unity/Assets/Conetent/Julian/Scripts/ObjectPooler.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+// Object Pooler Made By Julian Rijken
+public class ObjectPooler : MonoBehaviour
+{
+
+    [SerializeField] private Pool[] m_pools;
+
+    private static ObjectPooler m_instance;
+
+    private void Awake()
+    {
+
+        if (m_instance == null)
+        {
+            m_instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        // Add Start Size To Pools
+        for (int i = 0; i < m_pools.Length; i++)
+        {
+            m_pools[i].m_pooledObjects = new List<GameObject>();
+            AddToPool(m_pools[i],m_pools[i].m_startSize);
+        }
+
+    }
+
+
+    private GameObject AddToPool(Pool pool, int itmeCount = 1)
+    {
+        if (itmeCount < 1)
+            itmeCount = 1;
+
+        GameObject retunObject = null;
+
+        if (pool != null)
+        {
+
+            for (int i = 0; i < itmeCount; i++)
+            {
+                GameObject addedObject = Instantiate(pool.m_object, Vector3.zero, transform.rotation);
+
+                pool.m_pooledObjects.Add(addedObject);
+
+                addedObject.SetActive(false);
+                addedObject.transform.parent = transform;
+
+                retunObject = addedObject;
+            }
+        }
+
+        return retunObject;
+    }
+
+    private Pool GetPool(string poolName)
+    {
+        for (int i = 0; i < m_pools.Length; i++)
+        {
+            if (m_pools[i].m_name == poolName)
+                return m_pools[i];
+        }
+
+        return null;
+    }
+
+    private GameObject GetObjectFromPool(Pool pool)
+    {
+        if (pool != null)
+        {
+            for (int i = 0; i < pool.m_pooledObjects.Count; i++)
+            {
+                if (!pool.m_pooledObjects[i].activeInHierarchy)
+                    return pool.m_pooledObjects[i];
+            }
+
+            return AddToPool(pool);
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+
+    public static GameObject SpawnObject(string name,Vector3 postion,Quaternion rotation, bool active = true)
+    {
+        ObjectPooler objectPooler = m_instance;
+
+        Pool pool = objectPooler.GetPool(name);
+
+        if (pool != null)
+        {
+            GameObject item = objectPooler.GetObjectFromPool(pool);
+
+            item.transform.position = postion;
+            item.transform.rotation = rotation;
+            item.SetActive(active);
+
+            return item;
+        }
+        else
+        {
+            Debug.LogWarning("Pool: " + name + " does not exist");
+            return null;
+        }
+
+    }
+
+}
+
+[System.Serializable]
+public class Pool
+{
+    [HideInInspector]
+    public List<GameObject> m_pooledObjects;
+
+    public GameObject m_object;
+    public int m_startSize;
+    public string m_name;
+}
