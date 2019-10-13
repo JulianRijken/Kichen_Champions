b98b12555644941d7febe0bbd03f765fbf5fb5db
Julian <Rijkenjulian@gmail.com>
2019-10-13 22:43:00 +0200

    Game Loop Added + Scene Finder

---
+++ Xbox_PartyGame_Unity/Assets/Conetent/JulianRijken/Scripts/GameUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI timerText;
+
+    private void Awake()
+    {
+        MiniGameManager.Instance.OnTimerUpdate += HandleTimer;
+    }
+
+    private void OnDestroy()
+    {
+        MiniGameManager.Instance.OnTimerUpdate -= HandleTimer;
+    }
+
+    private void HandleTimer(float time)
+    {
+        if(timerText != null)
+        timerText.text = Mathf.Round(time).ToString();
+    }
+
+}
