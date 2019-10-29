dcdb3f2e9efee60d3a26c92ea1bacbfe45a1f069
Julian <Rijkenjulian@gmail.com>
2019-10-29 12:11:39 +0100

    Working on switching

---
+++ Xbox_PartyGame_Unity/Assets/Conetent/JulianRijken/Scripts/SceneSwitch/SceneNames.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NamedScenes", menuName = "New NamedScenes")]
+public class SceneNames : ScriptableObject
+{
+    [SerializeField]
+    private NamedScene[] m_namedScenes;
+
+    public string GetSceneName(SceneEnumName sceneEnumName)
+    {
+        for (int i = 0; i < m_namedScenes.Length; i++)
+        {
+            NamedScene sceneContainer = m_namedScenes[i];
+            if (sceneContainer.sceneEnumName == sceneEnumName)
+                return sceneContainer.sceneName;
+        }
+
+        Debug.LogWarning("Scene Does Not Exist In Container");
+        return "null";
+    }
+
+}
