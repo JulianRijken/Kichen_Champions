b6e44127038168527215d950da57e34592e0bfbc
Julian <Rijkenjulian@gmail.com>
2019-11-02 16:30:48 +0100

    Minigame Manager And player align seprete

--- Xbox_PartyGame_Unity/Assets/Conetent/JulianRijken/Scripts/MiniGameManager.cs
+++ Xbox_PartyGame_Unity/Assets/Conetent/JulianRijken/Scripts/MiniGameManager.cs
@@ -8,23 +8,11 @@ public class MiniGameManager : MonoBehaviour
 {
     public static MiniGameManager Instance { get; private set; }
 
-    [Header("Timer")]
     [SerializeField] private float m_minigameTime;
 
-    [Header("Players")]
-    [SerializeField] private GameObject[] m_players;
-    [SerializeField] private int m_playerCount;
-
-    [Header("Alignment")]
-    [SerializeField] private bool m_align;
-    [SerializeField] private bool m_Grid;
-
-    [SerializeField] private float m_unitsBitweenX;
-    [SerializeField] private float m_unitsBitweenY;
-
     private bool[] m_playersDone;
-    private bool timerDone;
-    private float timer;
+    private bool m_IsTimerDone;
+    private float m_timer;
 
 
     void Awake()
@@ -34,15 +22,11 @@ public class MiniGameManager : MonoBehaviour
         else
             Destroy(gameObject);
 
+        if(PlayerInputCenter.SelectedPlayerCount < 2)      
+            SceneLoader.LoadScene(SceneEnumName.ControllerSetup);
 
-        m_playerCount = PlayerInputCenter.SelectedPlayerCount;
+        m_playersDone = new bool[PlayerInputCenter.SelectedPlayerCount];
 
-        if(m_playerCount < 2)      
-            SceneLoader.LoadScene(SceneEnumName.ControllerSetup);       
-        else   
-            ResetPlayers();
-
-        m_playersDone = new bool[m_playerCount];
         for (int i = 0; i < m_playersDone.Length; i++)
         {
             m_playersDone[i] = false;
@@ -51,23 +35,23 @@ public class MiniGameManager : MonoBehaviour
 
     private void Start()
     {
-        timer = m_minigameTime;
-        timerDone = false;
+        m_timer = m_minigameTime;
+        m_IsTimerDone = false;
     }
 
     private void Update()
     {
-        if (!timerDone)
+        if (!m_IsTimerDone)
         {
-            timer -= Time.deltaTime;
+            m_timer -= Time.deltaTime;
 
-            if (timer <= 0)
+            if (m_timer <= 0)
             {
-                timer = 0;
-                timerDone = true;
-                FireTimerUpdate(timer);
-                FireTimerOver();
+                m_timer = 0;
+                m_IsTimerDone = true;
                 OnTimerOver();
+                FireTimerUpdate(m_timer);
+                FireTimerOver();
             }
         }
     }
@@ -90,79 +74,10 @@ public class MiniGameManager : MonoBehaviour
 
     private void LateUpdate()
     {
-        if (!timerDone)
-            FireTimerUpdate(timer);
+        if (!m_IsTimerDone)
+            FireTimerUpdate(m_timer);
     }
 
-
-
-    [ContextMenu("ResetPlayers")]
-    private void ResetPlayers()
-    {
-        for (int i = 0; i < m_players.Length; i++)
-            m_players[i].SetActive(false);
-
-        for (int i = 0; i < m_playerCount; i++)
-            m_players[i].SetActive(true);
-
-        if (m_align)
-        {
-            float centerX = Camera.main.transform.position.x;
-            float centerY = Camera.main.transform.position.y;
-            bool m_gridLocal = m_Grid;
-
-            if (m_playerCount == 2 && m_gridLocal) m_gridLocal = false;
-
-            if (m_gridLocal)
-            {
-                for (int i = 0; i < m_playerCount; i++)
-                {
-                    int iCopy = i;
-                    float ax = m_unitsBitweenX;
-                    float ay = m_unitsBitweenY;
-                    float b = m_playerCount;
-                    float posY = centerY + ay;
-                    if (m_gridLocal)
-                    {
-                        if (i >= 2)
-                        {
-                            iCopy = Mathf.CeilToInt(i / 2f) - 1;
-                            posY = centerY - ay;
-                        };
-
-                    }
-                    print(i+"I");
-                    float posX = centerX - ((ax * Mathf.Ceil(b/2f) - ax) / 2f) + ax * iCopy;
-
-                    if (m_playerCount == 3 && i == 2)
-                    {
-                        posX = centerX;
-                    }
-
-                    Vector2 newPos = new Vector2(posX, posY);
-
-                    if (m_players[i].GetComponent<RectTransform>() == null)
-                        m_players[i].transform.position = newPos;
-                }
-
-            }
-            else
-            {
-                for (int i = 0; i < m_playerCount; i++)
-                {
-                    float a = m_unitsBitweenX;
-                    float b = m_playerCount;
-                    float posX = centerX - ((a * b - a) / 2f) + a * i;
-
-                    Vector2 newPos = new Vector2(posX, transform.position.y);
-
-                    if (m_players[i].GetComponent<RectTransform>() == null)
-                        m_players[i].transform.position = newPos;
-                }
-            }
-
-        }
-    }
 
 
     public void FireTimerOver()
---
+++ Xbox_PartyGame_Unity/Assets/Conetent/JulianRijken/Scripts/PlayerAlignment.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Julian.InputSystem;
+using UnityEditor;
+
+public class PlayerAlignment : MonoBehaviour
+{
+
+    [SerializeField] private GameObject[] m_players;
+    [SerializeField] private int m_playerCount;
+    [SerializeField] private bool m_align;
+    [SerializeField] private bool m_Grid;
+    [SerializeField] private CenterType m_centerType;
+    [SerializeField] private float m_unitsBitweenX;
+    [SerializeField] private float m_unitsBitweenY;
+
+    private enum CenterType { camera, thisTransform}
+
+    void Awake()
+    {
+        m_playerCount = PlayerInputCenter.SelectedPlayerCount;
+        ResetPlayers();
+    }
+
+    [ContextMenu("Test Align")]
+    private void ResetPlayers()
+    {
+        for (int i = 0; i < m_players.Length; i++)
+            m_players[i].SetActive(false);
+
+        for (int i = 0; i < m_playerCount; i++)
+            m_players[i].SetActive(true);
+
+        if (m_align)
+        {
+            float centerX;
+            float centerY;
+
+            if (m_centerType == CenterType.camera)
+            {
+                centerX = Camera.main.transform.position.x;
+                centerY = Camera.main.transform.position.y;
+            }
+            else
+            {
+                RectTransform rectTransform = GetComponent<RectTransform>();
+                if (rectTransform == null)
+                {
+                    centerX = transform.position.y;
+                    centerY = transform.position.x;
+                }
+                else
+                {
+                    centerX = rectTransform.position.x;
+                    centerY = rectTransform.position.y;
+                }
+            }
+
+            bool m_gridLocal = m_Grid;
+
+            if (m_playerCount == 2 && m_gridLocal) m_gridLocal = false;
+
+            if (m_gridLocal)
+            {
+                for (int i = 0; i < m_playerCount; i++)
+                {
+                    int iCopy = i;
+                    float ax = m_unitsBitweenX;
+                    float ay = m_unitsBitweenY;
+                    float b = m_playerCount;
+                    float posY = centerY + ay;
+                    if (m_gridLocal)
+                    {
+                        if (i >= 2)
+                        {
+                            iCopy = Mathf.CeilToInt(i / 2f) - 1;
+                            posY = centerY - ay;
+                        };
+
+                    }
+                    print(i + "I");
+                    float posX = centerX - ((ax * Mathf.Ceil(b / 2f) - ax) / 2f) + ax * iCopy;
+
+                    if (m_playerCount == 3 && i == 2)
+                    {
+                        posX = centerX;
+                    }
+
+                    Vector2 newPos = new Vector2(posX, posY);
+
+                    RectTransform rectTransform = m_players[i].GetComponent<RectTransform>();
+                    if (rectTransform == null)
+                        m_players[i].transform.position = newPos;
+                    else
+                        rectTransform.position = newPos;
+
+                }
+
+            }
+            else
+            {
+                for (int i = 0; i < m_playerCount; i++)
+                {
+                    float a = m_unitsBitweenX;
+                    float b = m_playerCount;
+                    float posX = centerX - ((a * b - a) / 2f) + a * i;
+
+                    Vector2 newPos = new Vector2(posX, transform.position.y);
+
+                    RectTransform rectTransform = m_players[i].GetComponent<RectTransform>();
+                    if (rectTransform == null)
+                        m_players[i].transform.position = newPos;
+                    else
+                        rectTransform.position = newPos;
+                }
+            }
+
+        }
+    }
+
+}
