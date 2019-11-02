f4d3f4283f5c1008cd268fbeb4b095da206b0b0b
Julian <Rijkenjulian@gmail.com>
2019-11-02 22:20:59 +0100

    Exit Button Added

--- Xbox_PartyGame_Unity/Assets/Conetent/JulianRijken/Scripts/MainMenu.cs
+++ Xbox_PartyGame_Unity/Assets/Conetent/JulianRijken/Scripts/MainMenu.cs
@@ -4,8 +4,25 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+
+    /// <summary>
+    /// Starts the game
+    /// </summary>
     public void StartGame()
     {
         SceneLoader.LoadSceneAsync(SceneEnumName.ControllerSetup);
+    }
+
+    /// <summary>
+    /// Closes the application
+    /// </summary>
+    public void ExitApplication()
+    {
+
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+
+        Application.Quit();
     }
 }
