73f911809cde17c81113658550b70834eae8a638
VoidedBlade <jurian4bergen@live.nl>
2019-11-05 12:15:42 +0100

    Slice it Finishing up

--- Xbox_PartyGame_Unity/Assets/Conetent/Jurian/Scripts/SliceIt/SliceItInput.cs
+++ Xbox_PartyGame_Unity/Assets/Conetent/Jurian/Scripts/SliceIt/SliceItInput.cs
@@ -7,18 +7,17 @@ using UnityEngine.InputSystem;
 public class SliceItInput : MonoBehaviour
 {
     [SerializeField, Header("Required")] private int m_player;
-    [SerializeField] private Rigidbody2D cucumber;
+    [SerializeField] private GameObject cucumber;
 
     private float scale;
     private Vector2 m_MovementInput;
     private Vector3 input;
     private Vector3 startpos;
     private bool done;
-    private Vector3 topos;
 
     private int slices;
-
-
+    private bool AddPoint;
+    private bool debounce;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,31 +26,55 @@ public class SliceItInput : MonoBehaviour
         {
             PlayerInputCenter.PlayerInputEvents[m_player].OnMove += StickMovement;
         }
-        startpos = cucumber.position;
-        topos = startpos;
+        startpos = cucumber.transform.position;
         done = false;
+        slices = 0;
     }
 
 
     private void StickMovement(InputAction.CallbackContext conext)
     {
-        m_MovementInput = conext.ReadValue<Vector2>();
         if (!done)
-            MoveCucumber();
+            m_MovementInput = conext.ReadValue<Vector2>();
     }
 
     private void MoveCucumber()
     {
         input = new Vector3(m_MovementInput.x, 0, 0);
-        topos = Vector3.Lerp(startpos, startpos + input, Time.deltaTime);
-            
-        cucumber.MovePosition(Vector3.Lerp(cucumber.position, topos, Time.deltaTime * 3));
+        Vector3 topos = Vector3.Lerp(startpos, startpos + input, 10f);
+        cucumber.transform.position = Vector3.MoveTowards(cucumber.transform.position, Vector3.Lerp(cucumber.transform.position, topos, Time.deltaTime * 20), 10f);
+        if (m_MovementInput.x >= 0.5f && !debounce)
+        {
+            AddPoint = true;
+            debounce = true;
+        }
+        if (m_MovementInput.x <= -0.5f)
+        {
+            debounce = false;
+        }
     }
     private void OnDestroy()
     {
         if (PlayerInputCenter.PlayerExists(m_player))
         {
             PlayerInputCenter.PlayerInputEvents[m_player].OnMove -= StickMovement;
+        }
+    }
+    private void Update()
+    {
+        MoveCucumber();
+
+        if (AddPoint)
+        {
+            Debug.Log("sliced");
+            slices += 1;
+            AddPoint = false;
+        }
+
+        if (slices == 30 && !done)
+        {
+            done = true;
+            MiniGameManager.SetPlayerDone(m_player);
         }
     }
 }
