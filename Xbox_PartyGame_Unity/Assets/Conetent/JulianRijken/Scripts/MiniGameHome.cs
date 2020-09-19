5df3e3077d99cd28e1405dfb5e49f3ba67ed2ac5
Julian <rijkenjulian@gmail.com>
2020-09-19 17:12:50 +0200

    Minigame Home Code Cleanup

--- Xbox_PartyGame_Unity/Assets/Conetent/JulianRijken/Scripts/MiniGameHome.cs
+++ Xbox_PartyGame_Unity/Assets/Conetent/JulianRijken/Scripts/MiniGameHome.cs
@@ -7,17 +7,18 @@ using UnityEngine.UI;
 public class MiniGameHome : MonoBehaviour
 {
 
+    [Header("General")]
     [SerializeField] private TMP_Dropdown m_dropdown;
     [SerializeField] private SceneEnumName[] m_minigameScenes;
     [SerializeField] private TextMeshProUGUI[] m_scoreTexts;
     [SerializeField] private Image[] m_progresImage;
     [SerializeField] private List<PlayerData> m_checkPlayers;
     [SerializeField] private GameObject m_suddenDeathText;
-    
+
 
     [Header("GameSelect")]
-    [SerializeField] Sprite[] m_gameSprites;
-    [SerializeField] Image m_gameSpriteImage;
+    [SerializeField] private Sprite[] m_gameSprites;
+    [SerializeField] private Image m_gameSpriteImage;
     [SerializeField] private AudioClip tickClip;
     [SerializeField] private AudioSource m_tickSource;
     [SerializeField] private AnimationCurve m_slowDownCurve;
@@ -29,21 +30,14 @@ public class MiniGameHome : MonoBehaviour
     private int m_currentSelected;
     private bool gameSelected;
 
-
-    private List<string> m_minigameScenesString;
+    private List<string> m_minigameScenesString = new List<string>();
     private int m_selectedScene;
-    private bool m_sceneSwitched;
+    private bool m_sceneSwitched = false;
+
 
     private void Awake()
     {
         SceneLoader.OnSceneSwitched += OnSceneSwitched;
-
-        if (PlayerInputCenter.SelectedPlayerCount < 2)
-            SceneLoader.LoadScene(SceneEnumName.ControllerSetup);
-
-        m_sceneSwitched = false;
-        m_suddenDeathText.SetActive(false);
-
     }
 
     private void OnDestroy()
@@ -53,13 +47,14 @@ public class MiniGameHome : MonoBehaviour
 
     private void Start()
     {
+        if (PlayerInputCenter.SelectedPlayerCount < 2)
+            SceneLoader.LoadScene(SceneEnumName.ControllerSetup);
 
-        m_minigameScenesString = new List<string>();
+        m_suddenDeathText.SetActive(false);
 
         for (int i = 0; i < m_minigameScenes.Length; i++)
-        {
             m_minigameScenesString.Add(m_minigameScenes[i].ToString());
-        }
+
 
         m_dropdown.ClearOptions();
         m_dropdown.AddOptions(m_minigameScenesString);
@@ -68,6 +63,7 @@ public class MiniGameHome : MonoBehaviour
 
 
         m_checkPlayers = new List<PlayerData>();
+
         for (int i = 0; i < m_scoreTexts.Length; i++)
         {
             PlayerData playerData = GameManager.ScoreCenter.GetPlayerData(i);
@@ -75,7 +71,6 @@ public class MiniGameHome : MonoBehaviour
             {
                 if (playerData.m_score >= GameManager.ScoreCenter.GetGameLength())
                     m_checkPlayers.Add(playerData);
-
 
                 m_scoreTexts[i].text = playerData.m_score + " / " + GameManager.ScoreCenter.GetGameLength().ToString();
                 m_progresImage[i].fillAmount = 0;
@@ -97,10 +92,9 @@ public class MiniGameHome : MonoBehaviour
             for (int i = 0; i < m_scoreTexts.Length; i++)
             {
                 PlayerData playerData = GameManager.ScoreCenter.GetPlayerData(i);
+
                 if (playerData != null)
-                {
                     m_progresImage[i].fillAmount = Mathf.MoveTowards(m_progresImage[i].fillAmount, Mathf.Clamp01(playerData.m_score / (float)GameManager.ScoreCenter.GetGameLength()), Time.deltaTime * 0.5f);
-                }
             }
         }
 
@@ -109,7 +103,6 @@ public class MiniGameHome : MonoBehaviour
             m_swichTimer += Time.deltaTime / m_slowDownTime;
             m_swichTimer = Mathf.Clamp01(m_swichTimer);
             m_animatior.SetFloat("Progress", m_swichTimer);
-
 
             if (m_swichTimer == 1)
             {
@@ -120,7 +113,7 @@ public class MiniGameHome : MonoBehaviour
 
             m_swichSpeed += Time.deltaTime * m_slowDownCurve.Evaluate(m_swichTimer) * m_speedMultiplier;
 
-            if(m_swichSpeed >= 1f)
+            if (m_swichSpeed >= 1f)
             {
                 SwitchGame();
                 m_swichSpeed = m_swichSpeed - 1f;
@@ -129,43 +122,28 @@ public class MiniGameHome : MonoBehaviour
 
     }
 
+
     private void SwitchGame()
     {
         m_currentSelected++;
+
         if (m_currentSelected >= m_gameSprites.Length)
             m_currentSelected = 0;
+
         m_gameSpriteImage.sprite = m_gameSprites[m_currentSelected];
         m_tickSource.PlayOneShot(tickClip);
         m_animatior.SetTrigger("GameSwitched");
     }
 
-    public void StartCurrentGame()
-    {
-        SceneLoader.LoadSceneAsync(m_minigameScenes[m_currentSelected]);
-    }
-
-
-
-    private void OnSceneSwitched()
-    {
-        m_sceneSwitched = true;
-    }
-
-
     private void CheckWinner(List<PlayerData> playerData)
     {
-
-
-
         if (playerData.Count > 1)
         {
             int bestPlayerScore = 0;
             for (int i = 0; i < playerData.Count; i++)
             {
                 if (playerData[i].m_score > bestPlayerScore)
-                {
                     bestPlayerScore = playerData[i].m_score;
-                }
             }
 
             List<PlayerData> bestPlayers = new List<PlayerData>();
@@ -176,17 +154,21 @@ public class MiniGameHome : MonoBehaviour
             }
 
             if (bestPlayers.Count > 1)
-            {
-                Debug.Log("SuddenDeath");
                 m_suddenDeathText.SetActive(true);
-            }
-            else if(bestPlayers.Count == 1)
-            {
+            else if (bestPlayers.Count == 1)
                 OnGameFinish(bestPlayers[0]);
-            }
+
         }
         else if (playerData.Count == 1)
+        {
             OnGameFinish(playerData[0]);
+        }
+    }
+
+
+    private void OnSceneSwitched()
+    {
+        m_sceneSwitched = true;
     }
 
     private void OnGameFinish(PlayerData winner)
@@ -194,7 +176,6 @@ public class MiniGameHome : MonoBehaviour
         GameManager.ScoreCenter.SetWinningPlayer(winner);
         SceneLoader.LoadScene(SceneEnumName.EindScreen);
     }
-
 
 
     public void OnDropdownChanged(int option)
@@ -206,5 +187,10 @@ public class MiniGameHome : MonoBehaviour
     public void LoadScene()
     {
         SceneLoader.LoadSceneAsync(m_minigameScenes[m_selectedScene]);
+    }
+
+    public void StartCurrentGame()
+    {
+        SceneLoader.LoadSceneAsync(m_minigameScenes[m_currentSelected]);
     }
 }
